bd95b1f319acad570ae1bc8a01d277f37d45361c
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-22 12:58:15 +0100

    defines getters for merchant

--- models/merchant.py
+++ models/merchant.py
@@ -2,3 +2,9 @@ class Merchant:
     def __init__(self, name, id = None):
         self.name = name
         self.id = id
+
+def get_merchant_name(self):
+    return self.name
+
+def get_merchant_id(self):
+    return self.id

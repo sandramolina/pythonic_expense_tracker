2d8b9619979f4ec7c6f0041b68df2b093ce302aa
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-22 17:46:50 +0100

    adds delete cat function to controller

--- controllers/categories_controllers.py
+++ controllers/categories_controllers.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.category import Category
@@ -14,9 +15,14 @@ def categories():
 def new_human():
     return render_template("categories/new.html")
 
-@categories_bp.route("/categories", methods=["POST"])
+@categories_bp.route('/categories', methods=['POST'])
 def create_category():
     name = request.form['name']
     new_category = Category(name)
     category_repository.save(new_category)
+    return redirect('/categories')
+
+@categories_bp.route('/categories/<id>/delete', methods = ['POST'])
+def delete_category(id):
+    category_repository.delete(id)
     return redirect('/categories')

d0dfe01893e03a7068059432acc576312cf8dab1
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-22 12:57:10 +0100

    defines getters for category

--- models/category.py
+++ models/category.py
@@ -2,3 +2,9 @@ class Category:
     def __init__(self, name, id = None):
         self.name = name
         self.id = id
+
+def get_expense_name(self):
+    return self.name
+
+def get_expense_id(self):
+    return self.id

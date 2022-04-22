ba7d2d3d531e875429997491ac9a43a6fc3c5f74
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-22 13:00:47 +0100

    defines getters for expense

--- models/expense.py
+++ models/expense.py
@@ -6,3 +6,21 @@ class Expense:
         self.amount = amount
         self.description = description
         self.id = id
+
+def get_expense_date(self):
+    return self.date
+
+def get_expense_merchant(self):
+    return self.merchant
+
+def get_expense_category(self):
+    return self.category
+
+def get_expense_amount(self):
+    return self.amount
+
+def get_expense_description(self):
+    return self.description
+
+def get_expense_id(self):
+    return self.id

bb84593a74b70ddcc7e24da6215ee038acdc36b4
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-23 09:15:20 +0100

    adds create expense functionality

--- controllers/expenses_controller.py
+++ controllers/expenses_controller.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.merchant import Merchant
@@ -20,6 +21,23 @@ def new_expense():
     merchants = merchant_repository.select_all()
     categories = category_repository.select_all()
     return render_template('index.html', all_merchants = merchants, all_categories = categories)
+
+@expenses_bp.route('/', methods = ['POST'])
+def create_expense():
+    date = request.form['date']
+    description = request.form['description']
+    amount = request.form['amount']
+
+    merchant_id = request.form['merchant_id']
+    category_id = request.form['category_id']
+
+    merchant_object = merchant_repository.select(merchant_id)
+    category_object = category_repository.select(category_id)
+
+    new_expense = Expense(date, merchant_object, category_object, amount, description)
+    expense_repository.save(new_expense)
+
+    return redirect('/dashboard')
 
 
 

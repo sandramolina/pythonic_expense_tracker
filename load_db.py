8d642f9fe777d3ea027b186c017415ece89722c0
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-23 08:26:10 +0100

    renders expense details in the dashboard

--- load_db.py
+++ load_db.py
@@ -8,11 +8,11 @@ from models.expense import Expense
 import repositories.expense_repository as expense_repository
 
 #Testing save function
-# category1 = Category("Groceries")
-# category_repository.save(category1)
+category1 = Category("Groceries")
+category_repository.save(category1)
 
-# category2 = Category("Transportation")
-# category_repository.save(category2)
+category2 = Category("Transportation")
+category_repository.save(category2)
 
 #test select all 
 #print(category_repository.select_all()[1].__dict__)
@@ -24,11 +24,11 @@ import repositories.expense_repository as expense_repository
 #category_repository.delete(2)
 
 #Testing save function
-# merchant1 = Merchant("Tesco")
-# merchant_repository.save(merchant1)
+merchant1 = Merchant("Tesco")
+merchant_repository.save(merchant1)
 
-# merchant2 = Merchant("The Pony Inn")
-# merchant_repository.save(merchant2)
+merchant2 = Merchant("The Pony Inn")
+merchant_repository.save(merchant2)
 
 #test select all 
 #print(merchant_repository.select_all())
@@ -37,8 +37,8 @@ import repositories.expense_repository as expense_repository
 #merchant_repository.delete(2)
 
 #Testing save function
-# expense1 = Expense("2022-04-25", merchant2, category1, 120, "Dinner with Khal Drogo")
-#expense_repository.save(expense1)
+expense1 = Expense("2022-04-25", merchant2, category1, 120, "Dinner with Khal Drogo")
+expense_repository.save(expense1)
 
 #test select all 
 #print(expense_repository.select_all()[0].__dict__)
